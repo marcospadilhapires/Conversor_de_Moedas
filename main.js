dc153ede0fc69a7828f3337d808fbd05267bf517
Marcos <marcospadilhapires@gmail.com>
2023-11-15 17:16:19 -0300

    Editado o HTML E CSS e consumida outra API para consulta de moedas

--- main.js
+++ main.js
@@ -1,31 +1,91 @@
-var url ='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=%2711-04-2023%27&@dataFinalCotacao=%2711-14-2023%27&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
-
+const seletor1 = ['USD','EUR','GBP','JPY','AUD','CHF','CAD','CNY','ARS','TRY'];
+var moeda1, moeda2;
+var moeda1Seletor, moeda2Seletor;
+var moeda1Selecao, moeda2Selecao
+var urlparte1,urlparte2
 var dollarExchangeRate, realValue, userInput;
 
-
-function getDollarExchageRate(){
-    var dollarExchangeRate
-    return fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => {
-        return data.value[0].cotacaoCompra;
-    })
-    .catch((error) =>{
-        console.error('Ocorreu um erro:',error)
-        throw error;
-    });
+function obterParteURL(moedaSelecao){
+    var urlparte;
+    switch(moedaSelecao){
+        case 'dolar-americano':
+            urlparte = seletor1[0]
+            break
+        case 'euro':
+            urlparte = seletor1[1]
+            break
+        case 'libra-esterlina':
+            urlparte = seletor1[2]
+            break
+        case 'iene':
+            urlparte = seletor1[3]
+            break
+        case 'dolar-australiano':
+            urlparte = seletor1[4]
+            break
+        case 'franco-suico':
+            urlparte = seletor1[5]
+            break
+        case 'dolar-canadense':
+            urlparte = seletor1[6]
+            break
+        case 'renminbi(Yuan)':
+            urlparte = seletor1[7]
+            break
+        case 'peso-argentino':
+            urlparte = seletor1[8]
+            break
+        case 'lira-turca':
+            urlparte = seletor1[9]
+            break
+        default:
+            urlparte = 'Moeda não encontrada'
+    }
+    console.log(urlparte)
+    return urlparte;
 
 }
 
-getDollarExchageRate()
-.then((dollarExchangeRate) => {
-    userInput = 30;
-    realValue = dollarExchangeRate * userInput;
-    alert('O valor $'+userInput+' é igual a R$'+ realValue.toFixed(2))
+
+moeda1resultado = document.querySelector('#moeda-resultado-1');
+moeda2resultado = document.querySelector('#moeda-resultado-2');
+moeda1Seletor = document.querySelector('#cotacao1')
+moeda1Seletor.addEventListener('change',function(){
+    moeda1Selecao = moeda1Seletor.value;
+    urlparte1 = obterParteURL(moeda1Selecao)
+    if(urlparte1 && urlparte2){
+        var url =('https://economia.awesomeapi.com.br/last/'+urlparte1+'-'+urlparte2);
+        console.log(url)
+        getCoinExchageRate(url)
+    }
 })
-.catch((error) =>{
-    console.error('Erro ao obter a taxa de cambio:',error)
-});
+moeda2Seletor = document.querySelector('#cotacao2')
+moeda2Seletor.addEventListener('change',function(){
+    moeda2Selecao = moeda2Seletor.value;
+    urlparte2 = obterParteURL(moeda2Selecao)
+    if(urlparte1 && urlparte2){
+        var url =('https://economia.awesomeapi.com.br/last/'+urlparte1+'-'+urlparte2);
+        console.log(url)
+        getCoinExchageRate(url)
+    }
+})
+
+function getCoinExchageRate(url){
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+        var cotacoes = [];
+        cotacoes[0] = data[urlparte1+urlparte2].low
+        //cotacoes[1] = data[urlparte2+urlparte1].low
+        moeda1resultado.innerHTML = cotacoes[0];
+        //moeda2resultado.innerHTML = cotacoes[1];
+    })
+    .catch((error) =>{
+        console.error('Ocorreu um erro:',error)
+    });
+}
+
+
 
 
 
